72e1949d8b45d8e032925b46c86b3bb96102d13d
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-10 01:39:53 +0200

    start of login functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,59 @@ $(document).on("submit", "form.js-register", function(event) {
     })
     .done(function ajaxDone(data) {
         //What ever data is
+
+        if(data.redirect !== undefined) {
+            window.location = data.redirect;
+        } else if (data.error !== undefined) {
+            _error.text(data.error).show();
+        }
+
+    })
+    .fail(function ajaxFailed(e) {
+        // This failed
+
+    })
+    .always(function ajaxAlwaysDoThis(data) {
+        //Always do
+        console.log('Always');
+    })
+
+    return false; //Stays at the page
+})
+
+.on("submit", "form.js-login", function(event) {
+    event.preventDefault();
+
+    var _form = $(this);// form stored in variabel
+    var _error = $(".js-error", _form);
+
+    var dataObj = {
+        email: $("input[type='email']", _form).val(),
+        password: $("input[type='password']", _form).val()
+    };
+
+    if(dataObj.email.length < 6) {
+        _error.text("Please enter a valid email address").show();
+        return false;
+    } else if (dataObj.password.length < 11) {
+        _error
+            .text("Please enter a passphrase that is at least 11 characters long.")
+            .show();
+        return false;
+    }
+
+    //Assuming code get this far, we can start the ajax process
+    _error.hide();
+
+    $.ajax({
+        type: 'POST',
+        url: 'createphpajaxlogin/php_login_course/ajax/login.php',
+        data: dataObj,
+        dataType: 'json',
+        async: true,
+    })
+    .done(function ajaxDone(data) {
+        //What ever data is
         
         if(data.redirect !== undefined) {
             window.location = data.redirect;
