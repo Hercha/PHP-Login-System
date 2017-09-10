cb7e07bb0cbf5c5daabe5156e93eb6ca51ccca82
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-10 14:10:52 +0200

    something is broken

--- inc/functions.php
+++ inc/functions.php
@@ -18,27 +18,3 @@ function ForceDashboard() {
         // The user is not allowed here
     }
 }
-
-function FindUser($con, $email, $return_assoc = false) {
-    // Make sure the user does not exist.
-    $email = (string) Filter::String( $email );
-
-    $findUser = $con->prepare("SELECT user_id, password FROM users WHERE email =  LOWER(:email) LIMIT 1");
-    $findUser->bindParam(':email', $email, PDO::PARAM_STR);
-    $findUser->execute();
-
-    if($return_assoc) {
-        return $findUser->fetch(PDO::FETCH_ASSOC);
-    }
-
-    $user_found = (boolean) $findUser->rowCount();
-
-    return $user_found;
-
-    // if($findUser->rowCount() === 1) { // The above do the same thing
-    //     return true;
-    // }
-
-    // return false;
-
-}
